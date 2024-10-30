db23a0a50a5e753bd4f849ca0157c33ea70c77e3
Leny Patin <lpatin@c1r3p8.42lehavre.fr>
2024-10-30 18:19:26 +0100

    Ajout de tout les fichiers de la librairie pour github.

---
+++ ft_strdup.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strdup.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpatin <lpatin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 14:33:42 by lpatin            #+#    #+#             */
+/*   Updated: 2024/10/23 14:53:00 by lpatin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strdup(const char *s)
+{
+	char		*ptr;
+	int			i;
+
+	i = 0;
+	ptr = malloc(sizeof(char) * ft_strlen(s) + 1);
+	if (ptr == NULL)
+		return (NULL);
+	while (s[i])
+	{
+		ptr[i] = s[i];
+		i++;
+	}
+	ptr[i] = '\0';
+	return (ptr);
+}
---
+++ ft_substr.c
@@ -0,0 +1,32 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_substr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lpatin <lpatin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/21 15:52:49 by lpatin            #+#    #+#             */
+/*   Updated: 2024/10/23 14:42:50 by lpatin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*ptr;
+	size_t	lenstr;
+
+	if (!s)
+		return (NULL);
+	lenstr = ft_strlen(s);
+	if (start >= lenstr)
+		return (ft_strdup(""));
+	if (start + len > lenstr)
+		len = lenstr - start;
+	ptr = malloc(len + 1);
+	if (!ptr)
+		return (NULL);
+	ft_strlcpy(ptr, s + start, len + 1);
+	return (ptr);
+}
